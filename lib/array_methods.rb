e9c5a6c8adde90d0d2a700c8f927998601087283
Sean Eriksen <eriksensean@yahoo.com>
2019-10-22 20:16:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,8 +4,14 @@ end
 
 def find_max_value(array)
   # Add your solution here
+  
+  find_max_value([1, 2, 1, 3, 4, 3, 5, 4, 3, 2, 1])
+  
 end
 
 def find_min_value(array)
   # Add your solution here
+  
+  find_min_value([10, 5, 3, 7, 19, 1, 3, 10])
+  
 end
